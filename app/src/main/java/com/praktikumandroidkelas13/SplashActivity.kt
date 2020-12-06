9dbb6ed0894e9a9f5fd58e20380f57b8e49fdb9a
hadisantoso15 <hadisnt15@gmail.com>
2020-12-06 16:17:34 +0800

    pengenalan komponen dari layout ke kotlin, setOnClickListener, konversi nilai, splash activity

---
+++ app/src/main/java/com/praktikumandroidkelas13/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.praktikumandroidkelas13
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        supportActionBar?.hide()
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+        }, 2000)
+    }
+}

1641a445d06c0f91659f04e2c32e9825b90bac99
hadisantoso15 <hadisnt15@gmail.com>
2020-12-22 20:48:54 +0800

    perbaikan error

--- app/src/main/java/com/praktikumandroidkelas13/Student.kt
+++ app/src/main/java/com/praktikumandroidkelas13/Student.kt
@@ -1,8 +1,40 @@
 package com.praktikumandroidkelas13
 
+import android.os.Parcel
+import android.os.Parcelable
+
 data class Student (
-    val npm: String?,
-    val name: String?,
-    val ipk: Double,
-    val phoneNumber: String?
-)
+        val npm: String?,
+        val name: String?,
+        val ipk: Double,
+        val phoneNumber: String?
+) : Parcelable {
+    constructor(parcel: Parcel) : this(
+            parcel.readString(),
+            parcel.readString(),
+            parcel.readDouble(),
+            parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(npm)
+        parcel.writeString(name)
+        parcel.writeDouble(ipk)
+        parcel.writeString(phoneNumber)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Student> {
+        override fun createFromParcel(parcel: Parcel): Student {
+            return Student(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Student?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
--- app/src/main/java/com/praktikumandroidkelas13/StudentActivity.kt
+++ app/src/main/java/com/praktikumandroidkelas13/StudentActivity.kt
@@ -5,17 +5,17 @@ import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import android.widget.EditText as EditText1
+import android.widget.EditText
 
 class StudentActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_student)
 
-        val etNpm = findViewById<EditText1>(R.id.etNpm)
-        val etName = findViewById<EditText1>(R.id.etName)
-        val etIpk = findViewById<EditText1>(R.id.etIpk)
-        val etPhoneNumber = findViewById<EditText1>(R.id.etPhoneNumber)
+        val etNpm = findViewById<EditText>(R.id.etNpm)
+        val etName = findViewById<EditText>(R.id.etName)
+        val etIpk = findViewById<EditText>(R.id.etIpk)
+        val etPhoneNumber = findViewById<EditText>(R.id.etPhoneNumber)
         val btSend1 = findViewById<Button>(R.id.btSend1)
         val btSend2 = findViewById<Button>(R.id.btSend2)
         val btDial = findViewById<Button>(R.id.btDial)
